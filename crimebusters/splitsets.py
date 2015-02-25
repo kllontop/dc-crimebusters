2538fd459229ce5a1f0a8e66ede734ee91486de7
kllontop <kb.llontop@gmail.com>
2015-02-24 22:48:53 -0500

    created file for use in test, train, cross-validation

---
+++ crimebusters/splitsets.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 24 22:33 2015
+
+@author: kathleen_llontop
+"""
+def SplittingSet(dataset):
+''' Function 1: Takes in data and splits it first into a train set and a test set.
+Function 2: Takes train set and splits it into k-folds
+
+Output: one test set (20%) and multiple k-folds
